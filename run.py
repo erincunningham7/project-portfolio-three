d8a93a44c3a258242b22a70ce874ffb2cbaa02c8
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-05-29 11:16:43 +0000

    Create card object

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Playing cards
+# 29/05/2023
+
+class Card(object):
+    """
+    A playing card
+    """
+    RANKS = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["D", "H", "S", "C"]
+    def __init__(self, rank, suits):
+        self.rank = rank
+        self.suits = suits
+
+    def __str__(self):
+        rep = f"{self.rank}:{self.suits}"
+        return rep
